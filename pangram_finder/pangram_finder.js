137bdc5e2ffb8bbfbff2ccaab1250833cc7df437
DeuterGraves <puckles@gmail.com>
2018-10-17 16:58:41 +0100

    pangram detector complete and passing tests.

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,16 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase();
 }
 
 PangramFinder.prototype.isPangram = function () {
+  return this.alphabet.every(letter => this.phrase.includes(letter));
 
+  // console.log("this alphabet", this.alphabet); //array of the alphabet
+
+  // this.phrase.includes("y") //this is true
+  // need to cycle the alphabet through where "y" lives
 }
+
 
 module.exports = PangramFinder;
